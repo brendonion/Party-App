c6071b11d058d4e8e36ab2e7cac00429e775ccc8
Brendan Walker <brendan@freshworks.io>
2019-04-17 08:06:08 -0700

    Adjusts swipe position and removes unused files.

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import configureStore from './store/store';
 import Routes from './Routes';
-
-export const store = configureStore();
 
 class App extends Component {
   render() {
     return(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     );
   }
 }
